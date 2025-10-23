6b36dc6a603430a0ea8d7104b3aefcc26b2e08e5
Aitortita <aitor_ezcu@hotmail.com>
2025-10-23 12:23:53 +0200

    Modularized navBar with javascript

---
+++ web/src/util/navBar.js
@@ -0,0 +1,18 @@
+const navBar = document.createElement("navbar");
+
+navBar.innerHTML = `<header>
+                <div class="logo">
+                    <img class="logoIcon" src="./src/assets/Logo.png"/></image>
+                    <h1>QuickBank</h1>
+                </div>
+                <div class="logContainer">
+                    <a class="signIn" href="./src/views/sign_in.html">
+                        Sign in
+                    </a>
+                    <a class="signUp" href="./src/views/sign_up.html">
+                        Sign up
+                    </a>
+                </div>
+            </header>`;
+
+export default navBar;
