15230ec7ca2043988bf729b1ff513448a9b313f6
Aitortita <aitor_ezcu@hotmail.com>
2025-10-31 10:11:40 +0100

    Server error managing and updated Zip validation

--- web/src/util/create_customer.js
+++ web/src/util/create_customer.js
@@ -1,18 +1,13 @@
 async function create_customer (data) {
     let url = 'http://localhost:8080/CRUDBankServerSide/webresources/customer';
-    try {
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => console.log(response))
-    } catch (err) {
-        console.log(err)
-    }
+    return await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
 }
 
 export default create_customer;
