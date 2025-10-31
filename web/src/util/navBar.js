5ff48249beeb0ef898ef8656e0b38870843acd56
Aitortita <aitor_ezcu@hotmail.com>
2025-10-31 13:50:42 +0100

    Relative Paths changed to Absolute Paths

--- web/src/util/navBar.js
+++ web/src/util/navBar.js
@@ -1,15 +1,14 @@
 const navBar = document.createElement("navbar");
-
 navBar.innerHTML = `<header>
                 <div class="logo">
-                    <img class="logoIcon" src="./src/assets/Logo.png"/></image>
+                    <img class="logoIcon" src="/QuickBank/src/assets/Logo.png"/></image>
                     <h1>QuickBank</h1>
                 </div>
                 <div class="logContainer">
-                    <a class="signIn" href="./src/views/sign_in.html">
+                    <a class="signIn" href="/QuickBank/src/views/sign_in.html">
                         Sign in
                     </a>
-                    <a class="signUp" href="./src/views/sign_up.html">
+                    <a class="signUp" href="/QuickBank/src/views/sign_up.html">
                         Sign up
                     </a>
                 </div>
